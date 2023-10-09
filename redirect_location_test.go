6829c8588a557c338bdf3639d7806b9c66c9fc8f
wangkun <git@wangkun.vip>
2023-10-09 11:51:51 +0800

    fix error

--- redirect_location_test.go
+++ redirect_location_test.go
@@ -98,7 +98,7 @@ func TestDefaultHandling(t *testing.T) {
 			desc:           "No forwarded Prefix and absolute path",
 			locationBefore: "http://host:815/path",
 			expLocation:    "http://host:815/path",
-		}
+		},
 	}
 
 	config := &Config{
